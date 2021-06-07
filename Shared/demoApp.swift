8e086fe9d369a745b6de8914817ba0a18b779f51
Vlad Tomsa <tomsavlad90@gmail.com>
2021-06-07 17:28:46 +0300

    Initial Commit

---
+++ Shared/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  Shared
+//
+//  Created by Vlad Tomsa on 07.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
